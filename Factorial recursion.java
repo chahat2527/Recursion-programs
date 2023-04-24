83743b223a9fe8b5ad9a560369635fad297320d4
chahat2725 <anurag27.bhandari@simplilearn.net>
2023-04-24 19:08:36 +1000

    first commit

---
+++ Factorial recursion.java
@@ -0,0 +1,11 @@
+public class factorial {
+    public static void main(String[ args]){
+        int i, fact=1;
+        int number=5;
+        //it is factorial to calculation
+        for(i=1, i<numbers, i++) {
+            fact=fact*1;
+        }
+        System.out.println("Factorial of " + number + " is; "fact);
+    }
+}
